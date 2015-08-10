faa3ab53a4fd5da3d056d5f1a2e459c19a3e35b5
amalkov <anton.malkov@gmail.com>
2015-08-10 20:57:31 +0000

    Add url param of Plan to sign up form link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     def home
+        @basic_plan = Plan.find_by(name: 'basic')
+        @pro_plan = Plan.find_by(name: 'pro')
     end
     
     def about
