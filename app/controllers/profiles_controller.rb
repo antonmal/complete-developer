fc426699fa11e99088df058c13e7ffb1afa85731
amalkov <anton.malkov@gmail.com>
2015-08-15 17:12:01 +0000

    Add safeguards to prevent editing another user details

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
     
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
     def new
         # form where a user can fill out their OWN profile
         # only the currently logged it should be able to do this
@@ -42,6 +45,11 @@ private
     
     def profile_params
         params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
     
 end
