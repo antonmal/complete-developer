f6112e8f6d6d79d85b6e1429f001a953a56f540d
amalkov <anton.malkov@gmail.com>
2015-08-15 13:07:12 +0000

    Fix typo in the profile show controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,7 @@ class ProfilesController < ApplicationController
     
     def create
         @user = User.find( params[:user_id] )
-        @profile = @user.build_profile
+        @profile = @user.build_profile( profile_params )
         
         if @profile.save
           flash[:success] = "Profile Updated!"
